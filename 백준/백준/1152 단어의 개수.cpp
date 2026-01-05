d0fd184861459165bd6f914abf41cb8717a4a066
이동빈 <108942204+dongbinLee6240@users.noreply.github.com>
2026-01-05 21:20:00 +0900

    2026/01/05

--- 백준/백준/1152 단어의 개수.cpp
+++ 백준/백준/1152 단어의 개수.cpp
@@ -1,21 +1,51 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
+
+//int main()
+//{
+//    string str;
+//    vector<string> str1;
+//    int word = 0;
+//
+//    getline(cin, str);   // 공백 포함 입력
+//    str1.push_back(str);
+//
+//    for (int i = 0; i < str.size(); i++)
+//    {
+//        if (i == 0 && str[i] == ' '||i==str.size()-1 &&str[i]== ' ')
+//            continue;
+//
+//        if (str[i] == ' ')
+//            word++;
+//    }
+//
+//    cout << word + 1;
+//    return 0;
+//}
 
 int main()
 {
-	string str;
-	vector<string> str1;
-	int word = 0;
-	cin >> str;
-	str1.push_back(str);
+    int word = 0;
+    bool inWord = false;
+    string str;
 
-	/*for (int i = 0; i < str.size(); i++)
-	{
-		if (i==0 && str1[i] == " ")
-		{
-			word = 0;
-		}
-		if(str1)
-	}*/
+    getline(cin, str);
+
+    for (char c : str)
+    {
+        if (c != ' ' && !inWord)
+        {
+            word++;
+            inWord = true;
+        }
+        else if (c == ' ')
+        {
+            inWord = false;
+        }
+    }
+
+    cout << word;
+
 }
---
+++ 백준/백준/단어의 개수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	string str;
+
+	cin >> str;
+
+	int cnt = 0;
+	string cur;
+	string start = str.front();
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (start != " " && str[i] != " ")
+		{
+
+		}
+	}
+}
